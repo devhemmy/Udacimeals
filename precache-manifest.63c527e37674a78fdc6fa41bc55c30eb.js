aac12518c0d1a2f007fd52dde4eb0e28f7ecd4f5
Hemmy Bastawi <dev.ibrahembastawi@gmail.com>
2019-05-05 01:07:12 +0200

    Updates

---
+++ precache-manifest.63c527e37674a78fdc6fa41bc55c30eb.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8a7932786b9653a0698b29ba1bb5be38",
+    "url": "/koi20555/Udacimeals/index.html"
+  },
+  {
+    "revision": "aa86a2d7054cccaa8064",
+    "url": "/koi20555/Udacimeals/static/css/main.5492f017.chunk.css"
+  },
+  {
+    "revision": "1121605bbdccb0d1c40b",
+    "url": "/koi20555/Udacimeals/static/js/2.9931d8b5.chunk.js"
+  },
+  {
+    "revision": "aa86a2d7054cccaa8064",
+    "url": "/koi20555/Udacimeals/static/js/main.0fc50a17.chunk.js"
+  },
+  {
+    "revision": "51e5dfc55360642ad8ee",
+    "url": "/koi20555/Udacimeals/static/js/runtime~main.0b63fd02.js"
+  }
+]);
